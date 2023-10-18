b781b5a2448f968bafea30a92f2d9d7c66a82652
Annamalai1995 <s.annamalai1995@gmail.com>
2023-10-18 11:44:18 +0530

    overeloading,riding,operatoroverloading

---
+++ corepython/oops/overriding.py
@@ -0,0 +1,15 @@
+class sam:
+    def name(self):
+        print("sampk")
+class raja(sam):
+    def name(self):
+        #super().name()
+        print("raja")
+class arun(raja):
+    def name(self):
+        #super().name()
+        print("ARUN")
+s=arun()
+s.name()
+s1=raja()
+s1.name()
