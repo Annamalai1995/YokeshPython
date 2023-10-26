219b8afa2aa60f3754e5767e04832c272094471d
Annamalai1995 <s.annamalai1995@gmail.com>
2023-10-26 10:44:06 +0530

    Files

---
+++ corepython/oops/File/Read.py
@@ -0,0 +1,8 @@
+krish=open("Yoesh.txt","a")
+# print(krish.read())
+# print(krish.readable())
+# print(krish.writable())
+#print(krish.mode)
+krish.write("Happy the faces")
+print("Append")
+krish.close()
---
+++ corepython/oops/File/open.py
@@ -0,0 +1,4 @@
+data=open("./Yoesh.txt","w")
+print("Successfully",data.name)
+data.write("Started the New Journey")
+data.close()
